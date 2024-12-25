901a410e6dd2dc7bf92add91eb935a74bd795f3a
PengYuan <79000373+y1174804262@users.noreply.github.com>
2024-12-25 16:51:39 +0800

    Update resolver.py

--- cert_chain_resolver/resolver.py
+++ cert_chain_resolver/resolver.py
@@ -1,5 +1,5 @@
 from contextlib import closing
-
+from OpenSSL import crypto
 from cert_chain_resolver.models import CertificateChain, Cert
 
 try:
@@ -15,7 +15,7 @@ except NameError:
 try:
     from typing import Any, Optional
     from cert_chain_resolver.castore.base_store import CAStore
-except ImportError:  # pragma: no cover
+except ImportError:
     pass
 
 
@@ -27,7 +27,7 @@ def _download(url):
         return resp.read()
 
 
-def resolve(bytes_cert, _chain=None, root_ca_store=None):
+def resolve(bytes_cert, _chain=None, root_ca_store=None, cert_bytes=None):
     # type: (bytes, Optional[CertificateChain], Optional[CAStore]) -> CertificateChain
     """A recursive function that follows the CA issuer chain
 
@@ -40,24 +40,26 @@ def resolve(bytes_cert, _chain=None, root_ca_store=None):
     Returns:
         All resolved certificates in chain
     """
-    cert = Cert.load(bytes_cert)
 
+    cert = Cert.load(bytes_cert)
     if not _chain:
         _chain = CertificateChain()
-
+    if not cert_bytes:
+        cert_bytes = []
     if cert in _chain:
         # Prevent recursion in case the cert is self-referential
         return _chain
-
     _chain += cert
-
+    cert_bytes.append(bytes_cert)
     parent_cert = None
     if cert.ca_issuer_access_location:
         parent_cert = _download(cert.ca_issuer_access_location)
-
+        cert = crypto.load_certificate(crypto.FILETYPE_ASN1, parent_cert)  # 加载父证书
+        parent_cert = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)  # 转换为PEM格式
     if parent_cert:
-        return resolve(parent_cert, _chain=_chain, root_ca_store=root_ca_store)
+        return resolve(parent_cert, _chain=_chain, root_ca_store=root_ca_store, cert_bytes=cert_bytes)
     elif not _chain.root and root_ca_store:
-        _chain += root_ca_store.find_issuer(cert)
+        _bytes_cert = bytes(root_ca_store.find_issuer(cert)[0])
+        cert_bytes.append(_bytes_cert)
 
-    return _chain
+    return cert_bytes
